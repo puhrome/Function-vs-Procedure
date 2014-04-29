b18307eabd0b9e1c4af0e98ada6aedd646288244
puhrome <phirom.yim@fullsail.edu>
2014-04-29 17:56:45 -0400

    Function vs Procedure
    Explaining the difference of Function and Procedure in WebStorm

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/**
+ * Function vs Procedure Created by Phirom Yim on 4/29/14.
+ */
+
+//this is a function
+function calcArea(width, height) {//w=30, h=20
+    var area = width * height;//variable for area
+    return area;//what we are returning, function spitting info out
+    //returning in the argument
+}
+
+//This is a procedure
+function calcAreaP(width, height){
+    var area = width * height;
+    console.log(area);//no return
+}
